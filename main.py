fbe3ca628f0d81c7abdb66729ae421b2422ea96d
Enzoq2202 <enzoq2202@gmail.com>
2021-05-21 17:02:31 -0300

    .

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ pygame.init()
 tela = pygame.display.set_mode((1250,400))
 pygame.display.set_caption('T-rex Game')
 
-imagem_cidade = pygame.image.load('Imagens\Cidade.jpg')
+imagem_cidade = pygame.image.load('Imagens\Cidade.png')
 imagem_cidade = pygame.transform.scale(imagem_cidade, (1250, 400))
-imagem_floresta = pygame.image.load('Imagens\Floresta 2.jpg')
+imagem_floresta = pygame.image.load('Imagens\Floresta.png')
 imagem_floresta = pygame.transform.scale(imagem_floresta, (1250, 400))
-imagem_inverno = pygame.image.load('Imagens\Inverno.jpg')
+imagem_inverno = pygame.image.load('Imagens\Vulcão.png')
 imagem_inverno = pygame.transform.scale(imagem_inverno, (1250, 400))
-imagem_praia = pygame.image.load('Imagens\Praia.jpg')
+imagem_praia = pygame.image.load('Imagens\Praia.png')
 imagem_praia = pygame.transform.scale(imagem_praia, (1250, 400))
-imagem_velho = pygame.image.load('Imagens\Velho Oeste.jpg')
+imagem_velho = pygame.image.load('Imagens\Velho Oeste.png')
 imagem_velho = pygame.transform.scale(imagem_velho, (1250, 400))
 
-i = 0 #velocidade do jogo
+i = 0 #Responsável pelas transições
 limite = 1250 #limite da tela para começar uma nova imagem 
 muda_fundo = 0 #marca de transição
 game = True 
+clock = pygame.time.Clock() #Velocidade dos backgrounds
 
 while game:
  
@@ -38,7 +39,7 @@ while game:
         i -= 1
 
         muda_fundo += 8 #tempo de transição
-
+        clock.tick(450)
         pygame.display.update()
     
     elif muda_fundo > 20000 and muda_fundo <= 40000:
@@ -55,7 +56,7 @@ while game:
         i -= 1
 
         muda_fundo += 8
-
+        clock.tick(550)
         pygame.display.update()
 
     elif muda_fundo > 40000 and muda_fundo <= 60000:
@@ -72,7 +73,7 @@ while game:
         i -= 1
 
         muda_fundo += 8
-
+        clock.tick(1000)
         pygame.display.update()
 
     elif muda_fundo > 60000 and muda_fundo <= 80000:
@@ -89,7 +90,7 @@ while game:
         i -= 1
 
         muda_fundo += 8
-
+        clock.tick(700)
         pygame.display.update()
 
     elif muda_fundo > 80000 and muda_fundo <= 100000:
@@ -106,7 +107,7 @@ while game:
         i -= 1
 
         muda_fundo += 8
-
+        clock.tick(750)
         pygame.display.update()
 
     else:
