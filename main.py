5217630335b3a04fb462c337385d8a42b5dc7cb7
Joao Pedro <joaoprf4@al.insper.edu.br>
2021-05-26 16:13:29 -0300

    .

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ class Zilla:
             self.run_zilla = False
             self.duck_zilla = False 
             self.jump_zilla = True 
+            self.v_jump = 9.2
         elif Teclas[pygame.K_DOWN] and not self.jump_zilla: # Abaixa com a seta pra baixo pressionada
             self.run_zilla = False
             self.duck_zilla = True 
@@ -104,9 +105,9 @@ class Zilla:
         if self.jump_zilla:
             self.zilla_rect.y -= self.v_jump * 4
             self.v_jump -= 0.8
-        if self.v_jump < - self.V_jump:
+        if self.zilla_rect.y >= self.Y_position:
             self.jump_zilla = False
-            self.v_jump = self.V_jump
+            self.zilla_rect.y = self.Y_position
     
     def draw(self,tela):
         tela.blit(self.image, (self.zilla_rect.x,self.zilla_rect.y))
