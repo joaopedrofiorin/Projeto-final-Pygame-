2484d4a9850fa2cd0b61c7af221632a68a02f10d
Joao Pedro <joaoprf4@al.insper.edu.br>
2021-06-01 07:56:27 -0300

    .

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ imagem_cidade = pygame.image.load('Imagens\Backgrounds\Cidade.png').convert()
 imagem_cidade = pygame.transform.scale(imagem_cidade, (WIDTH, HEIGHT))
 imagem_floresta = pygame.image.load('Imagens\Backgrounds\Floresta.png').convert()
 imagem_floresta = pygame.transform.scale(imagem_floresta, (WIDTH, HEIGHT))
-imagem_inverno = pygame.image.load('Imagens\Backgrounds\Vulcão.png').convert()
-imagem_inverno = pygame.transform.scale(imagem_inverno, (WIDTH, HEIGHT))
+imagem_vulcao = pygame.image.load('Imagens\Backgrounds\Vulcão.png').convert()
+imagem_vulcao = pygame.transform.scale(imagem_vulcao, (WIDTH, HEIGHT))
 imagem_praia = pygame.image.load('Imagens\Backgrounds\Praia.png').convert()
 imagem_praia = pygame.transform.scale(imagem_praia, (WIDTH, HEIGHT))
 imagem_velho = pygame.image.load('Imagens\Backgrounds\Velho Oeste.png').convert()
@@ -32,16 +32,44 @@ zilla_start = pygame.image.load('Imagens\Dino\ZillaStart.png')
 zilla_dead = pygame.image.load('Imagens\Dino\ZillaDead.png')
 
 #obstáculos
-obstaculos_grandes=[pygame.image.load ('Imagens\Obstáculos\Cidade\Carro_Grande.png'),
-pygame.image.load ('Imagens\Obstáculos\Floresta\Arvore_Grande.png'),
-pygame.image.load ('Imagens\Obstáculos\Praia\Coqueiro_Grande.png'),
-pygame.image.load ('Imagens\Obstáculos\Velho Oeste\Cavalo_Grande.png'),
-pygame.image.load ('Imagens\Obstáculos\Vulcão\Pedras_Grande.png')]
-obstaculos_pequenos=[pygame.image.load ('Imagens\Obstáculos\Cidade\Semáforo_Pequeno.png'),
-pygame.image.load ('Imagens\Obstáculos\Floresta\Arvore_pequena.png'),
-pygame.image.load ('Imagens\Obstáculos\Praia\Bola_pequeno.png'),
-pygame.image.load ('Imagens\Obstáculos\Velho Oeste\Carrinho_Pequeno.png'),
-pygame.image.load ('Imagens\Obstáculos\Vulcão\Dino_Pequeno.png')]
+#Termina depois esse dici
+cenarios = {
+    'cidade':{
+        'imagem': imagem_cidade, 
+        'obstaculos': [
+            pygame.image.load ('Imagens\Obstáculos\Cidade\Carro_Grande.png'),
+            pygame.image.load ('Imagens\Obstáculos\Cidade\Semáforo_Pequeno.png')
+        ]
+    },
+    'floresta':{
+        'imagem': imagem_floresta,
+        'obstaculos':[
+            pygame.image.load ('Imagens\Obstáculos\Floresta\Arvore_Grande.png'),
+            pygame.image.load ('Imagens\Obstáculos\Floresta\Arvore_pequena.png')
+        ]
+    },
+    'vulcao':{
+        'imagem': imagem_vulcao,
+        'obstaculos':[
+            pygame.image.load ('Imagens\Obstáculos\Vulcão\Dino_Pequeno.png'),
+            pygame.image.load ('Imagens\Obstáculos\Vulcão\Pedras_Grande.png')
+        ]
+    },
+    'praia':{
+        'imagem': imagem_praia,
+        'obstaculos':[
+            pygame.image.load ('Imagens\Obstáculos\Praia\Bola_pequeno.png'),
+            pygame.image.load ('Imagens\Obstáculos\Praia\Coqueiro_Grande.png')
+        ]
+    },
+    'velho oeste':{
+        'imagem': imagem_velho,
+        'obstaculos':[
+            pygame.image.load ('Imagens\Obstáculos\Velho Oeste\Carrinho_Pequeno.png'),
+            pygame.image.load ('Imagens\Obstáculos\Velho Oeste\Cavalo_Grande.png')
+        ]
+    }
+}
 
 # Listas Zilla:
 RUNNING = [zilla_run1,zilla_run2]
@@ -52,13 +80,15 @@ DEADING = zilla_dead
 
 
 # Classe Zilla: 
-class Zilla:
+class Zilla(pygame.sprite.Sprite):
 
     X_position = 10 # posição X do zilla
     Y_position = 290 # posição Y do zilla
     y_duck_position = 320 #posicao y do zilla agachando
 
     def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+
         self.run_image = RUNNING # puxa as imagens de run 
         self.duck_image = DUCKING # puxa as imagens de duck
         self.jump_image = JUMPING # puxa as imagens de jump
@@ -69,12 +99,12 @@ class Zilla:
 
         self.step_index = 0 # Vai servir para animar o Zilla
         self.image = self.run_image[0] # Serve para inicializar a primeira imagem
-        self.zilla_rect = self.image.get_rect() # Faz o hitbox do Zilla
-        self.zilla_rect.x = self.X_position # Posição do hitbox
-        self.zilla_rect.y = self.Y_position # Posição do hitbox
+        self.rect = self.image.get_rect() # Faz o hitbox do Zilla
+        self.rect.x = self.X_position # Posição do hitbox
+        self.rect.y = self.Y_position # Posição do hitbox
 
     
-    def update(self,Teclas): # Funçao para atualizar o Zilla
+    def update(self): # Funçao para atualizar o Zilla
         if self.duck_zilla: # Atualiza o abaixar
             self.duck()
         if self.run_zilla: # Atualiza o correr
@@ -85,63 +115,43 @@ class Zilla:
         if self.step_index >= 10: # Conta os passos, para animar o Zilla
             self.step_index = 0 
 
-        if Teclas[pygame.K_UP] and not self.jump_zilla: # Pula com a seta pra cima pressionada
-            self.run_zilla = False
-            self.duck_zilla = False 
-            self.jump_zilla = True 
-            self.v_jump = 9.2
-        elif Teclas[pygame.K_DOWN] and not self.jump_zilla: # Abaixa com a seta pra baixo pressionada
-            self.run_zilla = False
-            self.duck_zilla = True 
-            self.jump_zilla = False
-        elif not (self.jump_zilla or Teclas[pygame.K_DOWN]): # Zilla continua a correr
-            self.run_zilla = True
-            self.duck_zilla = False 
-            self.jump_zilla = False
-
     def duck(self): 
         self.image = self.duck_image[self.step_index // 5]
-        self.zilla_rect = self.image.get_rect()
-        self.zilla_rect.x = self.X_position # Posição do hitbox
-        self.zilla_rect.y = self.y_duck_position # Posição do hitbox
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_position # Posição do hitbox
+        self.rect.y = self.y_duck_position # Posição do hitbox
         self.step_index += 1
 
     def run(self):
         self.image = self.run_image[self.step_index // 5]
-        self.zilla_rect = self.image.get_rect()
-        self.zilla_rect.x = self.X_position # Posição do hitbox
-        self.zilla_rect.y = self.Y_position # Posição do hitbox
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_position # Posição do hitbox
+        self.rect.y = self.Y_position # Posição do hitbox
         self.step_index += 1
 
     def jump(self):
         self.image = self.jump_image
         if self.jump_zilla:
-            self.zilla_rect.y -= self.v_jump * 4
+            self.rect.y -= self.v_jump * 4
             self.v_jump -= 0.8
-        if self.zilla_rect.y >= self.Y_position:
+        if self.rect.y >= self.Y_position:
             self.jump_zilla = False
-            self.zilla_rect.y = self.Y_position
+            self.rect.y = self.Y_position
     
     def draw(self,tela):
-        tela.blit(self.image, (self.zilla_rect.x,self.zilla_rect.y))
+        tela.blit(self.image, (self.rect.x,self.rect.y))
 
-class obstacle:
-
-    def __init__(self,image,type):
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self,image):
+        pygame.sprite.Sprite.__init__(self)
         self.image=image
-        self.type=type
-        self.rect=self.image[self.type].get_rect()
+        self.rect=self.image.get_rect()
         self.rect.x= WIDTH
     
     def update(self):
         self.rect.x -= speed_bg
         if self.rect.x < -self.rect.width:
-            obstacle.pop()
-
-    def draw(self,tela):
-        tela.blit(self.image[self.type],self.rect)
-
-class big_and_small_objects(obstacle):
+            self.kill()
 
 i = 0 #Responsável pelas transições
 limite = WIDTH #limite da tela para começar uma nova imagem 
@@ -151,32 +161,56 @@ points = 0 #Score do Jogo
 fonte = pygame.font.Font('Fontes\PressStart2P.ttf', 20) # Fonte usada na letra
 
 # Lista Background:
-lista_bg = [imagem_cidade,imagem_floresta,imagem_inverno,imagem_praia,imagem_velho]
+lista_bg = list(cenarios.keys())
 index_bg = 0
 speed_bg = 10 #velocidade do jogo
 clock = pygame.time.Clock()
 
-player = Zilla()
+all_sprites = pygame.sprite.Group()
+all_obstacles = pygame.sprite.Group()
 
-imagem_classe=[]
+player = Zilla()
+all_sprites.add(player)
+
 while game:
     clock.tick(30)
-    
     
     for eventos in pygame.event.get():
         if eventos.type == pygame.QUIT:
             game = False 
+
+    Teclas = pygame.key.get_pressed()
+
+    if Teclas[pygame.K_UP] and not player.jump_zilla: # Pula com a seta pra cima pressionada
+        player.run_zilla = False
+        player.duck_zilla = False 
+        player.jump_zilla = True 
+        player.v_jump = 9.2
+    elif Teclas[pygame.K_DOWN] and not player.jump_zilla: # Abaixa com a seta pra baixo pressionada
+        player.run_zilla = False
+        player.duck_zilla = True 
+        player.jump_zilla = False
+    elif not (player.jump_zilla or Teclas[pygame.K_DOWN]): # Zilla continua a correr
+        player.run_zilla = True
+        player.duck_zilla = False 
+        player.jump_zilla = False
 
     # Manipulação do Background
     if muda_fundo % 1000 == 0 and muda_fundo != 0:
         index_bg = (index_bg + 1) % len(lista_bg)
         speed_bg += 5
 
-    image_bg = lista_bg[index_bg]
-    imagem_classe.append=(image_bg)
+    cenario = cenarios[lista_bg[index_bg]]
+    image_bg = cenario['imagem']
+
+    while len(all_obstacles) < 2:
+        tipo = random.randint(0,len(cenario['obstaculos'])-1)
+        obstaculo = Obstacle(cenario['obstaculos'][tipo])
+        all_obstacles.add(obstaculo)
+        all_sprites.add(obstaculo)
 
     tela.fill((255,255,255))
-    Teclas = pygame.key.get_pressed()
+    
 
     tela.blit(image_bg, (i,0))
     tela.blit(image_bg, (limite+i,0))
@@ -186,8 +220,8 @@ while game:
 
     muda_fundo += 1 #tempo de transição
 
-    player.draw(tela) # Desenha o Zilla na tela
-    player.update(Teclas) # Atualiza posição do Zilla
+    all_sprites.draw(tela) # Desenha o Zilla na tela
+    all_sprites.update() # Atualiza posição do Zilla
     # Score
     points += int(speed_bg/5)
     text = fonte.render('Points: ' + str(points), False, (0,255,0) )
@@ -195,7 +229,7 @@ while game:
     textRect.center = (1100, 40)
     tela.blit(text, textRect)
 
-    imagem_classe.clear()
+    
 
     pygame.display.update()
 
